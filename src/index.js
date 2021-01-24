e487fe9656f07ca765a305d865c38cfb998f6a32
Bug <khanhnd.B18CN309@stu.ptit.edu.vn>
2021-01-24 22:01:59 +0700

    make it better

---
+++ src/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+require("dotenv").config();
+const axios = require("axios");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+let telegram_url =
+  "https://api.telegram.org/bot" +
+  process.env.TELEGRAM_API_TOKEN +
+  "/sendMessage";
+app.get("/", function (req, res) {
+  const { message } = req.body;
+  console.log("ss");
+  let reply = "Welcome to telegram bot";
+  sendMessage(telegram_url, message, reply, res);
+});
+function sendMessage(url, message, reply, res) {
+  axios
+    .post(url, { chat_id: "1527516281", text: reply })
+    .then((response) => {
+      console.log("Message posted");
+      res.end("ok");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+app.listen(3000, () => console.log("Telegram bot is listening on port 3000!"));
