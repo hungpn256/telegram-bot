f3b85a9460d887368fe4fedae88a474994b7eeb5
hungpn256 <phamnanghung.25@gmail.com>
2021-01-26 21:52:46 +0700

    sua dashboard bao cao su co

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -9,31 +9,31 @@ module.exports.getUpdates = (req, res) => {
   });
 };
 
-module.exports.autoRepMessage = (req, res) => {
+module.exports.autoRepMessage = async (req, res) => {
   console.log(req.body);
-  // const { message } = req.body;
-  // const { text } = message;
-  // let reply;
+  const { message } = req.body;
+  const { text } = message;
+  let reply;
   const url = telegram_url + "/sendMessage";
-  // if (text.match("/poll.*")) {
-  //   if (text.indexOf("q:") !== -1 && text.indexOf("a:") !== -1) {
-  //     const question = text.slice(text.indexOf("q:") + 2, text.indexOf("a:"));
-  //     let answers = text.slice(text.indexOf("a:") + 2);
-  //     answers = answers.split(",");
-  //     console.log("question: ", question);
-  //     console.log("answers: ", answers);
-  //     sendPoll(telegram_url, message.chat.id, question, answers);
-  //     reply = "Create poll successfully!!!!";
-  //     sendMessage(url, message.chat.id, reply);
-  //   } else
-  //     sendMessage(
-  //       url,
-  //       message.chat.id,
-  //       "vui lòng đúng format /poll q:... a:option1,option2,..."
-  //     );
-  // } else {
-  //   reply = checkMessage(text);
-  // }
+  if (text.match("/poll.*")) {
+    if (text.indexOf("q:") !== -1 && text.indexOf("a:") !== -1) {
+      const question = text.slice(text.indexOf("q:") + 2, text.indexOf("a:"));
+      let answers = text.slice(text.indexOf("a:") + 2);
+      answers = answers.split(",");
+      console.log("question: ", question);
+      console.log("answers: ", answers);
+      await sendPoll(telegram_url, message.chat.id, question, answers);
+      reply = "Create poll successfully!!!!";
+      sendMessage(url, message.chat.id, reply);
+    } else
+      sendMessage(
+        url,
+        message.chat.id,
+        "vui lòng đúng format /poll q:... a:option1,option2,..."
+      );
+  } else {
+    reply = checkMessage(text);
+  }
   sendMessage(url, req.body.message.chat.id, req.body);
   res.send(req.body);
 };
