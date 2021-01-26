ba17c828e11c202e434fc4a776c3e75a17aff2a5
hungpn256 <phamnanghung.25@gmail.com>
2021-01-26 21:43:27 +0700

    sua dashboard bao cao su co

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -10,30 +10,30 @@ module.exports.getUpdates = (req, res) => {
 };
 
 module.exports.autoRepMessage = (req, res) => {
-  const { message } = req.body;
-  const { text } = message;
   console.log(req.body);
-  let reply;
-  const url = telegram_url + "/sendMessage";
-  if (text.match("/poll.*")) {
-    if (text.indexOf("q:") !== -1 && text.indexOf("a:") !== -1) {
-      const question = text.slice(text.indexOf("q:") + 2, text.indexOf("a:"));
-      let answers = text.slice(text.indexOf("a:") + 2);
-      answers = answers.split(",");
-      console.log("question: ", question);
-      console.log("answers: ", answers);
-      sendPoll(telegram_url, message.chat.id, question, answers);
-      reply = "Create poll successfully!!!!";
-      sendMessage(url, message.chat.id, reply);
-    } else
-      sendMessage(
-        url,
-        message.chat.id,
-        "vui lòng đúng format /poll q:... a:option1,option2,..."
-      );
-  } else {
-    reply = checkMessage(text);
-  }
+  // const { message } = req.body;
+  // const { text } = message;
+  // let reply;
+  // const url = telegram_url + "/sendMessage";
+  // if (text.match("/poll.*")) {
+  //   if (text.indexOf("q:") !== -1 && text.indexOf("a:") !== -1) {
+  //     const question = text.slice(text.indexOf("q:") + 2, text.indexOf("a:"));
+  //     let answers = text.slice(text.indexOf("a:") + 2);
+  //     answers = answers.split(",");
+  //     console.log("question: ", question);
+  //     console.log("answers: ", answers);
+  //     sendPoll(telegram_url, message.chat.id, question, answers);
+  //     reply = "Create poll successfully!!!!";
+  //     sendMessage(url, message.chat.id, reply);
+  //   } else
+  //     sendMessage(
+  //       url,
+  //       message.chat.id,
+  //       "vui lòng đúng format /poll q:... a:option1,option2,..."
+  //     );
+  // } else {
+  //   reply = checkMessage(text);
+  // }
   res.send(req.body);
 };
 function sendPoll(url, user_id, question, answer) {
