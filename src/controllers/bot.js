9fdbb6316f98ec087932d76896daf4b887a5502e
Bug <khanhnd.B18CN309@stu.ptit.edu.vn>
2021-01-26 13:57:46 +0700

    add /time /date

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -22,8 +22,13 @@ function checkMessage(text) {
   if (text.match("^/lichhoc1.*$")) {
     reply = "replichhoc";
   } 
-  else if(text.match("^/time")){
-    reply = `${Date.getYear()} - ${Date.getMonth()} - ${Date.getDate()}`
+  else if(text.match("^/date.*$")){
+    const date = new Date()
+    reply = `Thứ ${date.getDay()}, ${(date.getDate()< 10? '0'+date.getDate(): date.getDate())} - ${(date.getMonth()+1< 10? '0'+(date.getMonth()+1): date.getMonth()+1)} - ${date.getFullYear()}`
+  }
+  else if(text.match("^/time.*$")){
+    const date = new Date()
+    reply = ` ${date.getHours()} : ${(date.getMinutes()< 10? '0'+date.getMinutes(): date.getMinutes())} : ${(date.getSeconds()< 10? '0'+date.getSeconds(): date.getSeconds())}`
   }
   else {
     reply = "gọi gì???";
