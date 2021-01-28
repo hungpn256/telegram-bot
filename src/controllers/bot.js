48d621155fc622e86f66a84cdb81780a8d9f6d91
hungpn256 <phamnanghung.25@gmail.com>
2021-01-28 21:16:11 +0700

    update

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -1,6 +1,6 @@
 const telegram_url = require("../config/urls").telegram_url;
 const axios = require("axios");
-const User = require('../models/users');
+const User = require("../models/users");
 
 module.exports.getUpdates = (req, res) => {
   const url = telegram_url + "/getUpdates";
@@ -12,83 +12,85 @@ module.exports.getUpdates = (req, res) => {
 
 module.exports.autoRepMessage = async (req, res) => {
   console.log(req.body);
-  const body = req.body
+  const body = req.body;
   const { message } = req.body;
   const { text } = message;
   const user_id = message.from.id;
   const chat_id = message.chat.id;
-  User.findOne({user_id}).then((user)=>{
-    
-    if(user){
-
-      let datas = user.datas || [];
-      if(typeof req.body !== 'undefined') {
-        datas = [...datas,req.body];
-        user.datas = datas;
+  User.findOne({ user_id })
+    .then((user) => {
+      if (user) {
+        let datas = user.datas || [];
+        if (typeof req.body !== "undefined") {
+          datas = [...datas, req.body];
+          user.datas = datas;
+        }
+        return user.save().then(() => console.log("Add successfully."));
+      } else {
+        const newUser = new User({ user_id });
+        newUser.datas = [req.body];
+        newUser.save((err) => {
+          if (err) console.log(err);
+          else console.log("Add new user successfully.");
+        });
       }
-      return user.save().then(() => console.log('Add successfully.'));
-    }
-    else{
-      const newUser = new User({user_id});
-      newUser.datas = [req.body];
-      newUser.save((err) => {
-        if(err) console.log(err);
-        else console.log('Add new user successfully.');
-      })
-    }
-  }).catch((err)=>console.log(err));
+    })
+    .catch((err) => console.log(err));
   let reply;
   const url = telegram_url + "/sendMessage";
   var list_poll = [];
   if (text.match("^/done.*$")) {
-    await User.findOne({user_id}).then((user)=>{
-      if(user){
-        user.datas.forEach(element => {
-          list_poll.push(element.message.text);
-        }); 
-      }
-    }).catch((err)=> console.log(err.message));
+    await User.findOne({ user_id })
+      .then((user) => {
+        if (user) {
+          user.datas.forEach((element) => {
+            list_poll.push(element.message.text);
+          });
+        }
+      })
+      .catch((err) => console.log(err.message));
     // console.log(list_poll)
     var ques = "";
     var options = [];
-    var si=0,di=0;
-    for(var index in list_poll){
-      
-      if(list_poll[index].match("^/poll.*$")){
+    var si = 0,
+      di = 0;
+    for (var index in list_poll) {
+      if (list_poll[index].match("^/poll.*$")) {
         si = parseInt(index);
       }
-      if(list_poll[index].match("^/option.*$")){
+      if (list_poll[index].match("^/option.*$")) {
         di = parseInt(index);
-        console.log(di,index);
+        console.log(di, index);
       }
     }
-    console.log(si,di);
-    for(var i = si;i <= di; i++){
-      if(list_poll[i].match("^/ques.*$")){
+    console.log(si, di);
+    for (var i = si; i <= di; i++) {
+      if (list_poll[i].match("^/ques.*$")) {
         ques = list_poll[i].slice(6);
       }
-      if(list_poll[i].match("^/option.*$")){
+      if (list_poll[i].match("^/option.*$")) {
         options.push(list_poll[i].slice(8));
       }
     }
-    if (options.length >= 2){
-      await User.findOne({user_id}).then((user)=>{
-        if(user){
-          user.datas = [];
-        }
-        return user.save().then(() => console.log('Delete successfully.'));
-      }).catch((err)=> console.log(err.message))
-      reply = "Create poll successfully."
-      await sendPoll(telegram_url,chat_id,ques,options);
+    if (options.length >= 2) {
+      await User.findOne({ user_id })
+        .then((user) => {
+          if (user) {
+            user.datas = [];
+          }
+          return user.save().then(() => console.log("Delete successfully."));
+        })
+        .catch((err) => console.log(err.message));
+      reply = "Create poll successfully.";
+      await sendPoll(telegram_url, chat_id, ques, options);
+    } else {
+      reply =
+        "Add option with /option ... or create poll with /done.\n Your options need at least 2.";
     }
-    else{
-      reply = "Add option with /option ... or create poll with /done.\n Your options need at least 2."
-    }
-    
   } else {
-    reply = checkMessage(text);
+    reply = checkMessage(message);
   }
-  sendMessage(url,chat_id,reply);
+  sendMessage(url, chat_id, reply);
   // sendMessage(url, req.body.message.chat.id, req.body);
   res.send(req.body);
 };
@@ -116,7 +118,8 @@ function sendMessage(url, user_id, reply) {
       console.log(error);
     });
 }
-function checkMessage(text) {
+function checkMessage(messenge) {
+  let { text } = messenge;
   let reply;
   if (text.match("^/lichhoc1.*$")) {
     reply = "replichhoc";
@@ -136,16 +139,15 @@ function checkMessage(text) {
     } : ${
       date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()
     }`;
-  } else if(text.match("^/poll.*$")){
-    reply = "Enter your question.\nStart with /ques ...."
-  } 
-  else if(text.match("^/ques.*$")){
-    reply = "Enter your options.\nStart with /option ...."
-  }
-  else if (text.match("^/option.*$")){
-    reply = "Add option with /option ... or create poll with /done."
-  }
-  else {
+  } else if (text.match("^/poll.*$")) {
+    reply = "Enter your question.\nStart with /ques ....";
+  } else if (text.match("^/ques.*$")) {
+    reply = "Enter your options.\nStart with /option ....";
+  } else if (text.match("^/option.*$")) {
+    reply = "Add option with /option ... or create poll with /done.";
+  } else if (text.match("^/id$")) {
+    reply = messenge.chat.id;
+  } else {
     reply = "gọi gì???";
   }
   return reply;
